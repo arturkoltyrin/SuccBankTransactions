95f93cb60c735758fb2861a905a0dd61622d7d81
Artur Koltyrin <koltyrinartur@gmail.com>
2024-09-20 21:57:57 +0500

    Добавлена функция get_date для форматирования даты

--- src/widget/__init__.py
+++ src/widget/__init__.py
@@ -5,16 +5,22 @@ def mask_account_card(info: str) -> str:
    parts = info.split()
    if len(parts) < 2:
        raise ValueError("Input must have both type and number.")
+   elif len(parts) == 2:
+       type_ = parts[0]
+       number = parts[1]
+       if "Счет" in type_:
+           return get_mask_account(number)
+       else:
+           return get_mask_card_number(number)
+   elif len(parts) > 2:
+       type_ = parts[0]
+       number = parts[2]
+       if "Visa" in type_:
+           return get_mask_card_number(number)
 
-   type_ = parts[0]
-   number = parts[1]
+def get_date(date_str: str) -> str:
+   dt = datetime.fromisoformat(date_str)
 
-   if "Счет" in type_:
-       return get_mask_account(number)
-   else:
-       return get_mask_card_number(number)
+   return dt.strftime("%d.%m.%Y")
 
-   def get_date(date_str: str) -> str:
-       dt = datetime.fromisoformat(date_str)
-
-       return dt.strftime("%d.%m.%Y")
+print(mask_account_card('Счет 73654108430135874305'))
