7c6d7ee1a63177aa192cad58c276e8ea6133004d
Artur Koltyrin <koltyrinartur@gmail.com>
2024-10-21 18:48:48 +0500

    выполнено логирование модулей utils и masks

--- src/masks.py
+++ src/masks.py
@@ -1,33 +1,44 @@
+import logging
+
+
+logger = logging.getLogger("widget.masks")
+logger.setLevel(logging.DEBUG)
+
+file_handler = logging.FileHandler("C:/Users/user/PycharmProjects/SuccBankTransactions/logs/masks.log")
+file_formatter = logging.Formatter('%(asctime)s - %(module)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+
+
 def get_mask_card_number(info: str) -> str:
     """Маскирует номер карты или счета, оставляя видимыми первые 6 и последние 4 цифры."""
-
-    # Разделение входной строки на части
+    logger.debug(f"Попытка маскировки номера карты: {info}")
     parts = info.split()
-
-    # Проверка, что входная строка содержит как минимум тип и номер
     if len(parts) < 2:
+        logger.error("Ошибка: Входные данные должны содержать название карты или счета и номер.")
         raise ValueError("Входные данные должны содержать название карты или счета и номер.")
 
-    # Объединяем все части, кроме последней в тип
     type_card = " ".join(parts[:-1])
-    number = parts[-1]  # Последний элемент - это номер
+    number = parts[-1]
 
     if "Счет" in type_card:
-        # Если тип - счет, маскируем номер
-        return f"**{number[-4:]}"
+        masked = f"<strong>{number[-4:]}"
+        logger.info(f"Маскирование номера счета: {masked}")
+        return masked
     else:
-        # Если это карта, маскируем номер карты
         if len(number) > 10:
             vis_part = number[:6] + number[-4:]
             masked_part = "*" * (len(number) - 10)
             masked_card_number = f"{vis_part[:4]} {vis_part[4:6]}{masked_part[:2]} {masked_part[:4]} {vis_part[-4:]}"
-            return f"{masked_card_number}"
+            logger.info(f"Маскирование номера карты: {masked_card_number}")
+            return masked_card_number
         else:
-            # Обработать случай с номером карты, меньше или равным 10 символам
-            return f"{number}"
-
+            logger.info(f"Короткий номер карты, возвращается без маски: {number}")
+            return number
 
 def get_mask_account(account_number: str) -> str:
     """Маскирует номер счета, оставляя видимыми последние 4 цифры."""
+    masked = f"</strong>{account_number[-4:]}"
+    logger.info(f"Маскирование номера счета: {masked}")
 
-    return f"**{account_number[-4:]}"
+    return masked
--- src/utils/load_json.py
+++ src/utils/load_json.py
@@ -1,19 +1,34 @@
 import json
 import os
+import logging
 from typing import List, Dict, Any
+
+
+logger = logging.getLogger("utils.load_json")
+logger.setLevel(logging.DEBUG)
+
+file_handler = logging.FileHandler("C:/Users/user/PycharmProjects/SuccBankTransactions/logs/load_json.log")
+file_formatter = logging.Formatter('%(asctime)s - %(module)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
 
 
 def load_transactions(filepath: str) -> List[Dict[str, Any]]:
     """Загружает финансовые транзакции из JSON-файла."""
+    logger.debug(f"Попытка загрузить файл: {filepath}")
     if not os.path.exists(filepath) or not os.path.isfile(filepath):
+        logger.error(f"Файл не найден или не является файлом: {filepath}")
         return []
 
     with open(filepath, "r", encoding="utf-8") as file:
         try:
             data = json.load(file)
             if isinstance(data, list):
+                logger.info("Файл успешно загружен.")
                 return data
+            logger.warning("Данные в файле не являются списком.")
             return []
-        except (json.JSONDecodeError, TypeError):
+        except (json.JSONDecodeError, TypeError) as e:
+            logger.error(f"Ошибка декодирования JSON: {e}")
 
             return []
