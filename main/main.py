d2d616100628a5ea218b31fb12142fcfc7d8dfdd
Artur Koltyrin <koltyrinartur@gmail.com>
2024-10-31 19:33:14 +0500

    Изменен модуль main

--- main/main.py
+++ main/main.py
@@ -9,7 +9,7 @@ from src.processing.processing import filter_by_state, sort_by_date
 from src.reading_financial_transactions_from_CSV_XLSX_files.read_financ_transactions import load_transactions_from_csv, \
     load_transactions_from_excel
 from src.utils.load_json import load_transactions
-from src.masks import get_mask_card_number, get_mask_account
+from src.widget.mask_account_card import mask_account_card, get_date
 
 
 @log()
@@ -24,18 +24,12 @@ def filter_transactions_by_description(
 @log()
 def count_transactions_by_category(transactions: List[Dict[str, str]], categories: List[str]) -> Dict[str, int]:
     """Подсчитывает количество операций в каждой категории."""
-    count_dict = dict.fromkeys(categories, 0)
+    count_dict = Counter()
     for transaction in transactions:
         category = transaction.get('description', '').lower()
-        if category in count_dict:
+        if category in categories:
             count_dict[category] += 1
-    return count_dict
-
-
-@log()
-def filter_rub_transactions(transactions: List[Dict[str, Union[str, float]]]) -> List[Dict[str, Union[str, float]]]:
-    """Фильтрует транзакции, оставляя только рублевые."""
-    return [transaction for transaction in transactions if transaction.get('currency_code') == 'RUB']
+    return dict(count_dict)
 
 
 @log()
@@ -64,53 +58,60 @@ def main() -> None:
         return
 
     print("Файл успешно загружен.")
-
+    print(transactions)
     # Фильтрация по статусу
     valid_statuses = {"EXECUTED", "CANCELED", "PENDING"}
 
-    filtered_transactions = None
     while True:
         status = input("Введите статус (EXECUTED, CANCELED, PENDING): ").upper()
         if status in valid_statuses:
-            print(f'Операции отфильтрованы по статусу "{status}"')
+            print(f'Операции отфильтрованы по статусу "{status.upper()}"')
             filtered_transactions = filter_by_state(transactions, status)
+            print(filtered_transactions)
             break
         else:
             print(f'Статус операции "{status}" недоступен.')
-
+    print(filtered_transactions)
+    # Сортировка по дате
+    sort_choice = input("Отсортировать операции по дате? (Да/Нет): ").strip().lower()
+    if sort_choice == 'да':
+        order = input(
+            "Отсортировать по возрастанию или по убыванию? (по возрастанию/по убыванию): ").strip().lower()
+        if order == 'по убыванию':
+            filtered_transactions = sort_by_date(filtered_transactions, reverse=True)  # Передаем reverse=True
+        else:
+            filtered_transactions = sort_by_date(filtered_transactions)  # По умолчанию reverse=False
+    print(filtered_transactions)
     # Фильтрация по рублевым транзакциям
-    filtered_transactions = filter_rub_transactions(filtered_transactions)
-
-    # Конвертация валюты
-    convert_currency = input("Конвертировать суммы в рубли? (Да/Нет): ").strip().lower()
-    if convert_currency == 'да':
-        for trans in filtered_transactions:
-            trans['amount'] = convert_to_rub(trans)
-
+    currency_choice = input("Выводить только рублевые транзакции? Да/Нет: ")
+    if currency_choice.lower() == "да":
+        filtered_transactions = list(filter_by_currency(filtered_transactions, "RUB"))
+    print(filtered_transactions)
     # Фильтрация по описанию
     description_filter = input("Отфильтровать список транзакций по слову в описании? (Да/Нет): ").strip().lower()
     if description_filter == 'да':
         search_string = input("Введите строку для поиска в описании: ")
         filtered_transactions = filter_transactions_by_description(filtered_transactions, search_string)
-
-    # Сортировка по дате
-    sort_choice = input("Отсортировать операции по дате? (Да/Нет): ").strip().lower()
-    if sort_choice == 'да':
-        order = input("Отсортировать по возрастанию или по убыванию? (по возрастанию/по убыванию): ").strip().lower()
-        if order == 'по убыванию':
-            filtered_transactions = sort_by_date(filtered_transactions, reverse=True)  # Передаем reverse=True
-        else:
-            filtered_transactions = sort_by_date(filtered_transactions)  # По умолчанию reverse=False
-
+    print(filtered_transactions)
     # Вывод итогового списка транзакций
     if filtered_transactions:
         print("Распечатываю итоговый список транзакций...")
-        for trans in filtered_transactions:
-            masked_card = get_mask_card_number(f"Карта {trans['from']}")
-            masked_account = get_mask_account(trans['to'])
-            print(
-                f"\n{trans['date']} {trans['description']}\nСчет {masked_card} ➜ {masked_account}\nСумма: {trans['amount']} {trans['currency_code']}")
         print(f"Всего банковских операций в выборке: {len(filtered_transactions)}")
+        for trans in filtered_transactions:
+            try:
+                masked_from = mask_account_card(trans.get("from", ""))
+            except ValueError:
+                masked_from = "Не указан"
+
+            try:
+                masked_to = mask_account_card(trans.get("to", ""))
+            except ValueError:
+                masked_to = "Не указан"
+
+            print(f"""{get_date(trans['date'])} {trans['description']}\n
+            {masked_from} ➜ {masked_to}
+            Сумма: {trans["operationAmount"]['amount']} {trans["operationAmount"]["currency"]["code"]}
+        """)
     else:
         print("Не найдено ни одной транзакции, подходящей под ваши условия фильтрации.")
 
